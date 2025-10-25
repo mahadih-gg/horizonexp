529fd93c60630a6a02d071876c01493cfbf2bcab
mahadih <mahadih.dev@gmail.com>
2025-10-25 17:26:55 +0600

    Refactor Footer component for improved layout and responsiveness
    - Updated the Footer component to enhance layout with responsive design adjustments.
    - Reorganized social links and added new padding values for better spacing.
    - Removed duplicate import of `navLinks` for cleaner code.

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,3 +1,4 @@
+import { navLinks } from "@/navData";
 import Link from "next/link";
 import LinkedinIcon from "../icons/linkedin-icon";
 import PinterestIcon from "../icons/pinterest-icon";
@@ -5,7 +6,6 @@ import TiktokIcon from "../icons/tiktok-icon";
 import XIcon from "../icons/x-icon";
 import YoutubeIcon from "../icons/youtube-icon";
 import BrandLogo from "../svg/BrandLogo";
-import { navLinks } from "@/navData";
 
 const socialLinks = [
   {
@@ -37,23 +37,23 @@ const socialLinks = [
 
 const Footer = () => {
   return (
-    <footer className="bg-primary py-20">
+    <footer className="bg-primary pt-20 md:pt-20 pb-[60px] md:pb-20">
       <div className='container text-white'>
-        <div className="flex items-center justify-between">
+        <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-10 md:gap-0">
           <Link href="/">
             <BrandLogo variant="white" />
           </Link>
 
-          <div className="flex items-center gap-7">
+          <div className="w-full sm:w-auto flex items-center justify-between sm:justify-end gap-7">
             {navLinks?.map((link, index) => (
               <Link key={index} href={link.href} className="text-white text-sm 2xl:text-lg">
                 {link.label}
               </Link>
             ))}
           </div>
-
         </div>
-        <div className="flex items-center justify-between py-10">
+
+        <div className="flex flex-col md:flex-row items-start md:items-center justify-between gap-10 md:gap-0 py-10">
           <p className="text-white text-sm 2xl:text-lg">
             Backed by Cloudflare Workers Launchpad Program <br />
             All rights reserved by Flagship Technology Inc.
