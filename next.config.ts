d9501ad899b528d4b1ad3d7d4ca779301e596289
mahadih <mahadih.dev@gmail.com>
2025-11-04 15:18:02 +0600

    Add image configuration to Next.js for remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "horizonexp.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  }
+
 };
 
 export default nextConfig;
