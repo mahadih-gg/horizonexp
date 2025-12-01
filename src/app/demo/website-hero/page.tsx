8fb9297c55f649a61a0efc577bef27ad9d6a92b1
mahadih <mahadih.dev@gmail.com>
2025-12-01 13:44:57 +0600

    Enhance demo pages with new components and layout improvements
    - Introduced RightArrowIcon component for enhanced visual feedback on buttons.
    - Updated layout with new headings and improved spacing for better user engagement.
    - Added buttons for "Buy now" and "Free trial" with consistent styling across demo pages.
    - Enhanced responsiveness and visual consistency in the WebsiteExplainer and WebsiteHero demo pages.

--- src/app/demo/website-hero/page.tsx
+++ src/app/demo/website-hero/page.tsx
@@ -1,18 +1,36 @@
 'use client';
 
+import RightArrowIcon from '@/components/icons/right-arrow-icon';
+import { Button } from '@/components/ui/button';
 import { EntryPoint, EntryPointSize, EntryPointType } from '@thinkflagship/horizon-web-shorts';
 
 const WebsiteHeroDemoPage = () => {
   return (
-    <section>
-      <div className="py-8 md:py-[45px] 2xl:py-[60px]">
+    <section className='pt-[100px] md:pt-20 2xl:pt-[109px] max-w-[716px] 2xl:max-w-[955px] mx-auto'>
+      <h1 className='text-center text-[56px] 2xl:text-[77px] font-medium leading-none tracking-[-0.02em]'>
+        Your product/<br className='block sm:hidden' />service <br className='hidden sm:block' /> head<span className='inline sm:hidden'>-</span><br className='block sm:hidden' />line goes here
+      </h1>
+      <div className="py-10 md:py-8 2xl:py-11">
         <EntryPoint
           id="#935370"
           skeletonType={EntryPointType.CIRCLE}
           skeletonSize={EntryPointSize.BOLD}
         />
       </div>
-      <div className="bg-secondary min-h-[572px] md:min-h-[393px] 2xl:min-h-[524px]" />
+
+      <p className='text-center text-xl 2xl:text-[28px] text-primary-text font-medium'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.
+      </p>
+
+      <div className='flex items-center justify-center gap-2.5 2xl:gap-5 py-10 md:py-8 2xl:py-11'>
+        <Button className='w-1/2 md:w-[180px] 2xl:w-[242px] h-[60px] 2xl:h-[78px] text-lg 2xl:text-2xl'>
+          Buy now
+        </Button>
+        <Button variant="secondary" className='w-1/2 md:w-[180px] 2xl:w-[242px] h-[60px] 2xl:h-[78px] text-lg 2xl:text-2xl'>
+          Free trial
+          <RightArrowIcon />
+        </Button>
+      </div>
     </section>
   );
 };
