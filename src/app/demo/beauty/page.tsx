c7c512ad73ad70d544e8e5abb271b3dd633cb3a6
mahadih <mahadih.dev@gmail.com>
2025-12-11 12:55:29 +0600

    updated beauty

--- src/app/demo/beauty/page.tsx
+++ src/app/demo/beauty/page.tsx
@@ -13,13 +13,13 @@ const BeautyDemoPage = () => {
 
   return (
     <section className='pt-8 md:pt-[45px] 2xl:pt-[60px]'>
-      <div className='pb-8 md:pb-[30px] 2xl:pb-10'>
+      {/* <div className='pb-8 md:pb-[30px] 2xl:pb-10'>
         <EntryPoint
           id="#348249"
           skeletonType={EntryPointType.CIRCLE}
           skeletonSize={EntryPointSize.BOLD}
         />
-      </div>
+      </div> */}
 
       <Image
         src={isMobile ? "/assets/images/demo/beauty/1-sm.webp" : "/assets/images/demo/beauty/1.webp"}
@@ -32,9 +32,9 @@ const BeautyDemoPage = () => {
       <div className="pt-8 md:pt-[45px] 2xl:pt-[37px]">
         <h2 className='text-left text-xl 2xl:text-[28px] font-medium pb-4 2xl:pb-5'>Trending Now</h2>
         <EntryPoint
-          id="#089379"
+          id="#781647"
           skeletonType={EntryPointType.RECTANGLE}
-          skeletonSize={EntryPointSize.STANDARD}
+          skeletonSize={EntryPointSize.BOLD}
         />
       </div>
     </section>
